6b9ad33bafcad36642e0032582a4ca0467397652
Adrian Calton <adrian.calton@fostermade.co>
2024-02-09 13:15:40 +1100

    Switch to GTM from GA

--- app/ui/analytics.tsx
+++ app/ui/analytics.tsx
@@ -1,10 +1,10 @@
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function Analytics() {
     return (
         <>
-            {(process.env.GOOGLE_ANALYTICS_ID) && (
-                <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
+            {(process.env.GOOGLE_TAG_MANAGER_ID) && (
+                <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID} />
             )}
         </>
     );
