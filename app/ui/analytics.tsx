36910c004ccbc716bf1563e50352d61f4f2b84dd
Adrian Calton <adrian.calton@fostermade.co>
2024-02-13 14:47:59 +1100

    Add custom GA event

--- app/ui/analytics.tsx
+++ app/ui/analytics.tsx
@@ -1,4 +1,5 @@
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 export default function Analytics() {
     return (
@@ -6,6 +7,8 @@ export default function Analytics() {
             {(process.env.GOOGLE_TAG_MANAGER_ID) && (
                 <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID} />
             )}
+
+            <Script src="/test.js" strategy="afterInteractive" />
         </>
     );
 }
---
+++ public/test.js
@@ -0,0 +1,8 @@
+if (!window.dataLayer) {
+    window.dataLayer = [];
+}
+
+dataLayer.push({
+    'page_title': 'Testing my page title!',
+    'page_location': '/gallery/foobar'
+});
